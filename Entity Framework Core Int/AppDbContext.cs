23aa509672933dff8876081be90fb323c2a20980
NaTaN-19 <52347201+NaTaN-19@users.noreply.github.com>
2026-09-15 22:52:04 +0400

    Update AppDbContext.cs

--- Entity Framework Core Int/AppDbContext.cs
+++ Entity Framework Core Int/AppDbContext.cs
@@ -1,6 +1,10 @@
 public class AppDbContext : DbContext
 {
     public DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=****;Database=OnlineStore;Trusted_Connection=True;TrustServerCertificate=True;")
 }
 
 
